27dd327d137bce476a93ee61ddebcc5b8c95b684
Turtlemay <am@trtl.in>
2021-06-04 01:02:41 -0400

    Changed build command.

--- Foyfile.js
+++ Foyfile.js
@@ -1,7 +1,7 @@
 const { fs, task } = require("foy");
 
 task("build", async (ctx) => {
-	await ctx.exec("webpack");
+	await ctx.exec("webpack --mode=production");
 	await fs.mkdirp("./dist");
 	await ctx.exec("zip -D -r ./dist/turtlemay-target.zip ./css ./out ./manifest.json");
 });
