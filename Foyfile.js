193aff3a7ab58ca1089c512b82c3511b2bde5a7d
Turtlemay <am@trtl.in>
2021-06-28 01:42:46 -0400

    Added version to file name.

--- Foyfile.js
+++ Foyfile.js
@@ -1,7 +1,8 @@
 const { fs, task } = require("foy");
+const manifestJson = require("./manifest.json");
 
 task("build", async (ctx) => {
 	await ctx.exec("webpack --mode=production");
 	await fs.mkdirp("./dist");
-	await ctx.exec("zip -D -r ./dist/turtlemay-target.zip ./css ./out ./manifest.json");
+	await ctx.exec(`zip -D -r ./dist/turtlemay-target-${manifestJson.version}.zip ./css ./out ./manifest.json`);
 });
