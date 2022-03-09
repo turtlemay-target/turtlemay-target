22a5062991c687ad7919f10a1b62d708d427ab9e
Turtlemay <am@trtl.in>
2022-03-09 01:57:58 -0500

    Output release zip file.

--- Foyfile.js
+++ Foyfile.js
@@ -4,5 +4,7 @@ const manifestJson = require("./manifest.json");
 task("build", async (ctx) => {
 	await ctx.exec("webpack --mode=production");
 	await fs.mkdirp("./dist");
-	await ctx.exec(`zip -D -r ./dist/turtlemay-target-${manifestJson.version}.zip ./css ./out ./manifest.json`);
+	const outZip = `./dist/turtlemay-target-${manifestJson.version}.zip`;
+	await ctx.exec(`zip -D -r ${outZip} ./css ./out ./manifest.json`);
+	await fs.copy(outZip, './dist/turtlemay-target.zip');
 });
