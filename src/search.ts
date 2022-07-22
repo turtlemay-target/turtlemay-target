5f1939c6c7a079606f8b907be003b94e47c5573d
Turtlemay <am@trtl.in>
2022-07-21 23:31:54 -0400

    Cached element reference.

--- src/search.ts
+++ src/search.ts
@@ -4,31 +4,32 @@
 
 addEventListener("keydown", onKeyDown);
 
+let inputEl: HTMLInputElement | null;
+
 function onKeyDown(event: KeyboardEvent) {
 	const RESET_KEY = "`";
 
-	const elem: HTMLInputElement | null =
-		document.querySelector("input#search");
+	inputEl ??= document.querySelector("input#search");
 
-	if (!elem) {
+	if (!inputEl) {
 		return;
 	}
 
 	if (event.key === RESET_KEY) {
 		event.preventDefault();
-		elem.focus();
-		elem.value = "";
+		inputEl.focus();
+		inputEl.value = "";
 	}
 
 	// Focus input if not focused.
 	if (document.activeElement?.nodeName !== "INPUT") {
 		if (event.key === "Enter") {
 			event.preventDefault();
-			elem.focus();
-			elem.value = "";
+			inputEl.focus();
+			inputEl.value = "";
 		}
 		else if (event.key.match(/^([a-zA-Z])$/)?.[1] && !event.ctrlKey && !event.altKey) {
-			elem.focus();
+			inputEl.focus();
 		}
 	}
 }
