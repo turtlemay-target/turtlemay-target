8c92537d60decb27421f2c73fb8687560a8b20e3
Turtlemay <am@trtl.in>
2022-07-22 01:05:22 -0400

    Check for possible locations.

--- src/pickresult.ts
+++ src/pickresult.ts
@@ -5,11 +5,12 @@
 const numberTest = /^[0-9]$/i;
 
 addEventListener("keydown", (event: KeyboardEvent) => {
-	if (!location.search.startsWith("?searchTerm=")) {
-		return;
-	}
-
-	if (numberTest.test(event.key)) {
+	const locationCheck = (
+		location.search.startsWith("?searchTerm=") ||
+		location.pathname.startsWith("/c/") ||
+		location.pathname.startsWith("/pl/")
+	);
+	if (locationCheck && numberTest.test(event.key)) {
 		const elems = document.querySelectorAll('div[class^="ProductCardImage__Content"] a') as NodeListOf<HTMLAnchorElement>;
 		const n = Number(event.key);
 		const i = n === 0 ? 9 : n - 1;
