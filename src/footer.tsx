0a255d9ba9dafd62887832fa9eb4793ce1f62a49
Turtlemay <am@trtl.in>
2022-07-22 01:59:11 -0400

    Adding info to site footer.

---
+++ src/footer.tsx
@@ -0,0 +1,27 @@
+/**
+ * @file Add our info to the site footer.
+ */
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import packageJson from "../package.json";
+import manifestJson from "../manifest.json";
+import { waitForElement } from "../lib/util";
+
+waitForElement('div[data-test="@web/component-footer/SubFooter"] p[data-test="@web/component-footer/PrivacyDate"]').then(el => {
+	const myEl = document.createElement("p");
+	myEl.classList.add("h-text-white");
+	myEl.classList.add("h-padding-r-tight");
+
+	el.insertAdjacentElement("beforebegin", myEl);
+
+	ReactDOM.render(
+		<span className="turtlemay__footerText">
+			<a className="Link__StyledLink-sc-4b9qcv-0 ghasId" href={packageJson.repository} target="_blank">
+				{manifestJson.name}
+			</a>
+			&nbsp;{manifestJson.version}
+		</span>,
+		myEl
+	);
+});
