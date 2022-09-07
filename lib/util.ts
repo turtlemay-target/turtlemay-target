24302c3c7a62de3e7fc5195ffaccacb4a3aa3d76
Turtlemay <am@trtl.in>
2022-09-07 11:40:11 -0400

    Using outer scope.

--- lib/util.ts
+++ lib/util.ts
@@ -1,12 +1,12 @@
 export function waitForElement(selector: string): Promise<Element> {
 	return new Promise((resolve, reject) => {
-		const elem = document.querySelector(selector);
+		let elem = document.querySelector(selector);
 		if (elem) {
 			resolve(elem);
 			return;
 		}
 		new MutationObserver((mutationRecords, observer) => {
-			const elem = document.querySelector(selector);
+			elem ??= document.querySelector(selector);
 			if (elem) {
 				resolve(elem);
 				observer.disconnect();
