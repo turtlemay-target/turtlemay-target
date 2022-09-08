d1f3d72ff99a7d86b6e52bb6953d8e9e05a65630
Turtlemay <am@trtl.in>
2022-09-08 18:28:22 -0400

    Render only once and insert element if removed.

--- src/footer.tsx
+++ src/footer.tsx
@@ -7,43 +7,37 @@ import { createRoot } from "react-dom/client";
 import packageJson from "../package.json";
 import manifestJson from "../manifest.json";
 
-const footerAdjacentElemSelector = (
-	'div[data-test="@web/component-footer/SubFooter"] ' +
-	'a[data-test="@web/component-footer/LegalLink"]:last-of-type'
-);
-
 const myEl = document.createElement("p");
 myEl.className = "h-text-white h-padding-r-tight";
-
 const reactRoot = createRoot(myEl);
 
-render();
+reactRoot.render(
+	<span className="turtlemay__footerText">
+		<a className="Link__StyledLink-sc-4b9qcv-0 ghasId" href={packageJson.repository} target="_blank">
+			<span className="turtlemay__footerTextIcon">🧩</span>
+			<span>&nbsp;</span>
+			<span>{manifestJson.name}</span>
+		</a>
+		<span>&nbsp;</span>
+		<span className="turtlemay__footerTextVersion">{manifestJson.version}</span>
+	</span>
+);
+
+insertFooterElem(myEl);
 
 new MutationObserver((mutations, observer) => {
 	if (!document.body.contains(myEl)) {
-		render();
+		insertFooterElem(myEl);
 	}
 }).observe(document.body, {
 	childList: true,
 	subtree: true,
 });
 
-async function render(adjacentElem = document.querySelector(footerAdjacentElemSelector)) {
-	if (!adjacentElem) {
-		return;
-	}
-
-	adjacentElem.insertAdjacentElement("afterend", myEl);
-
-	reactRoot.render(
-		<span className="turtlemay__footerText">
-			<a className="Link__StyledLink-sc-4b9qcv-0 ghasId" href={packageJson.repository} target="_blank">
-				<span className="turtlemay__footerTextIcon">🧩</span>
-				<span>&nbsp;</span>
-				<span>{manifestJson.name}</span>
-			</a>
-			<span>&nbsp;</span>
-			<span className="turtlemay__footerTextVersion">{manifestJson.version}</span>
-		</span>
+function insertFooterElem(elem: Element) {
+	const adjacentElem = document.querySelector(
+		'div[data-test="@web/component-footer/SubFooter"] ' +
+		'a[data-test="@web/component-footer/LegalLink"]:last-of-type'
 	);
+	adjacentElem?.insertAdjacentElement("afterend", elem);
 }
