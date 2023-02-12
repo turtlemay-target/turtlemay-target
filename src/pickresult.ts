0759ccd68a3b07193ac32419eb0b78bd6c7b71d0
Turtlemay <am@trtl.in>
2023-02-12 00:44:39 -0500

    Hiding sponsored items from search results.

--- src/pickresult.ts
+++ src/pickresult.ts
@@ -10,7 +10,7 @@ addEventListener("keydown", (event: KeyboardEvent) => {
 		location.pathname.startsWith("/pl/")
 	);
 	if (locationCheck && numberTest.test(event.key)) {
-		const elems = document.querySelectorAll('div[class^="ProductCardImage__Content"] a') as NodeListOf<HTMLAnchorElement>;
+		const elems = document.querySelectorAll('div[class*="StyledCol"]:not(:has(p[data-test="sponsoredText"])) div[class^="ProductCardImage__Content"] a') as NodeListOf<HTMLAnchorElement>;
 		const n = Number(event.key);
 		const i = n === 0 ? 9 : n - 1;
 		const clickEl: HTMLAnchorElement | undefined = elems[i];
