78590d23bac0c170477983276431ec879e3f7820
Turtlemay <am@trtl.in>
2023-08-22 11:04:48 -0400

    Improved type syntax.

--- src/pickresult.ts
+++ src/pickresult.ts
@@ -10,10 +10,12 @@ addEventListener("keydown", (event: KeyboardEvent) => {
 		location.pathname.startsWith("/pl/")
 	);
 	if (locationCheck && numberTest.test(event.key)) {
-		const elems = document.querySelectorAll('div[class*="StyledCol"]:not(:has(p[data-test="sponsoredText"])) div[class^="ProductCardImage__Content"] a') as NodeListOf<HTMLAnchorElement>;
+		const elems = document.querySelectorAll<HTMLAnchorElement>('div[class*="StyledCol"]:not(:has(p[data-test="sponsoredText"])) div[class^="ProductCardImage__Content"] a');
+
 		const n = Number(event.key);
 		const i = n === 0 ? 9 : n - 1;
-		const clickEl: HTMLAnchorElement | undefined = elems[i];
+
+		const clickEl = elems?.[i] as HTMLAnchorElement | undefined;
 		clickEl?.click();
 	}
 });
