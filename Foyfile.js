4460af52ae7e9d5650c168883144f7b517e58a2e
Turtlemay <am@trtl.in>
2023-10-25 04:23:28 -0400

    Creating build directory first.

--- Foyfile.js
+++ Foyfile.js
@@ -2,8 +2,8 @@ const { fs, task } = require("foy");
 const manifestJson = require("./manifest.json");
 
 task("build", async (ctx) => {
-	await ctx.exec("webpack --mode=production");
 	await fs.mkdirp("./dist");
+	await ctx.exec("webpack --mode=production");
 	const outZip = `./dist/turtlemay-target-${manifestJson.version}.zip`;
 	await ctx.exec(`zip -D -r ${outZip} ./css ./out ./manifest.json`);
 	await fs.copy(outZip, "./dist/turtlemay-target.zip");
