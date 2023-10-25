10b143c360f25dd00d12afbb1d76a32d7b786589
Turtlemay <am@trtl.in>
2023-10-25 03:44:05 -0400

    Implemented math expression evaluation in search field.

--- scripts/content.ts
+++ scripts/content.ts
@@ -3,6 +3,7 @@ import "./footer";
 import "./product-barcode";
 import "./search-field";
 import "./open-result";
+import "./input-math";
 
 import "../css/style.css";
 import "../css/cleanup.css";
---
+++ scripts/input-math.tsx
@@ -0,0 +1,141 @@
+/**
+ * @file Process and render math expressions from input fields.
+ */
+
+import * as React from "react";
+import * as math from "mathjs";
+import { Root, createRoot } from "react-dom/client";
+
+let rootEl: HTMLElement | undefined;
+let reactRoot: Root | undefined;
+
+initMathWidget();
+
+void new MutationObserver(initMathWidget)
+	.observe(document.body, { childList: true, subtree: true });
+
+function initMathWidget() {
+	if (!rootEl) {
+		rootEl = document.createElement("div");
+		rootEl.className = "turtlemay__mathWidgetRoot";
+	}
+	if (!reactRoot) {
+		reactRoot = createRoot(rootEl);
+		reactRoot.render(React.createElement(MathWidget));
+	}
+	if (!document.body.contains(rootEl)) {
+		document.body.append(rootEl);
+	}
+}
+
+function MathWidget() {
+	const [active, setActive] = React.useState(false);
+	const [inputValue, setInputValue] = React.useState("");
+	const [mathResult, setMathResult] = React.useState("");
+	const [renderValue, setRenderValue] = React.useState("");
+
+	React.useEffect(mutationObserver, []);
+	React.useEffect(focusInListener, [mathResult]);
+	React.useEffect(focusOutListener, []);
+	React.useEffect(changedInputValue, [inputValue]);
+	React.useEffect(changedMathResult, [mathResult]);
+
+	return (
+		<div className="turtlemay__mathWidgetContainer" tabIndex={-1} onClick={() => setActive(false)}>
+			<div className="turtlemay__mathWidget" data-turtlemay-active={active}>
+				<span className="turtlemay__mathWidgetEqualsSign">=</span>
+				<span className="turtlemay__mathWidgetResult turtlemay__enterAnimation" key={renderValue}>
+					{renderValue}
+				</span>
+			</div>
+		</div>
+	);
+
+	function mutationObserver() {
+		const observer = new MutationObserver(onMutation);
+		observer.observe(document.body, { childList: true, subtree: true, attributes: true });
+		return () => observer.disconnect();
+
+		function onMutation(mutations: MutationRecord[]) {
+			for (const v of mutations) {
+				// Detect changed input value using our own attribute.
+				const changedValue = (
+					v.target.nodeName === "INPUT" &&
+					v.target === document.activeElement &&
+					v.attributeName === "turtlemay-value"
+				);
+
+				if (changedValue) {
+					const inputEl = v.target as HTMLInputElement;
+					setInputValue(inputEl.value);
+					break;
+				}
+			}
+		}
+	}
+
+	function focusInListener() {
+		document.addEventListener("focusin", onFocusIn);
+		return () => document.removeEventListener("focusin", onFocusIn);
+
+		function onFocusIn() {
+			if (document.activeElement?.nodeName === "INPUT") {
+				const inputEl = document.activeElement as HTMLInputElement;
+
+				// Assign our input event callback to detect changed value.
+				inputEl.addEventListener("input", onInputCallback);
+
+				setInputValue(inputEl.value);
+
+				if (mathResult) {
+					setActive(true);
+				}
+			}
+		}
+	}
+
+	function focusOutListener() {
+		document.addEventListener("focusout", onFocusOut);
+		return () => document.removeEventListener("focusout", onFocusOut);
+
+		function onFocusOut() {
+			if (document.activeElement?.nodeName !== "INPUT") {
+				setActive(false);
+			}
+		}
+	}
+
+	function changedInputValue() {
+		setMathResult("");
+
+		// Check if the string looks like a math expression.
+		if (!inputValue.match(/.*\D+.*/)) {
+			return;
+		}
+
+		let mathEval: unknown;
+		try { mathEval = math.evaluate(inputValue); } catch (err) { }
+
+		if (typeof mathEval === "string" || typeof mathEval === "number") {
+			setMathResult(String(mathEval));
+		}
+	}
+
+	function changedMathResult() {
+		if (mathResult) {
+			setActive(true);
+			setRenderValue(mathResult);
+		} else {
+			setActive(false);
+		}
+		if (document.activeElement?.nodeName !== "INPUT") {
+			setActive(false);
+		}
+	}
+
+	/** Set value attribute on input elements to be used by mutation observer. */
+	function onInputCallback(event: Event) {
+		const inputEl = event.target as HTMLInputElement;
+		inputEl.setAttribute("turtlemay-value", inputEl.value);
+	}
+}
