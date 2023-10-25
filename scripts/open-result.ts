7b66e89cc6de494e0b8d542a393afd9c27695e9d
Turtlemay <am@trtl.in>
2023-10-24 21:52:00 -0400

    Whitespacing.

--- scripts/open-result.ts
+++ scripts/open-result.ts
@@ -4,11 +4,13 @@
 
 addEventListener("keydown", (event: KeyboardEvent) => {
 	const numberTest = /^[0-9]$/i;
+
 	const locationCheck = (
 		location.search.startsWith("?searchTerm=") ||
 		location.pathname.startsWith("/c/") ||
 		location.pathname.startsWith("/pl/")
 	);
+
 	if (locationCheck && numberTest.test(event.key)) {
 		const elems = document.querySelectorAll<HTMLAnchorElement>('div[class*="StyledCol"]:not(:has(p[data-test="sponsoredText"])) div[class^="ProductCardImage__Content"] a');
 
@@ -16,7 +18,7 @@ addEventListener("keydown", (event: KeyboardEvent) => {
 		const i = n === 0 ? 9 : n - 1;
 
 		const clickEl = elems?.[i] as HTMLAnchorElement | undefined;
-		
+
 		// Hold ctrl to open in new window.
 		if (event.ctrlKey && clickEl?.href) {
 			window.open(clickEl.href, "_blank");
