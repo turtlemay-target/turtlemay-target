237ac6d41be6d3624ca283d1987c99823794a27a
Turtlemay <am@trtl.in>
2023-10-25 04:31:18 -0400

    Clean output directories before building.

--- Foyfile.js
+++ Foyfile.js
@@ -2,8 +2,14 @@ const { fs, task } = require("foy");
 const manifestJson = require("./manifest.json");
 
 task("build", async (ctx) => {
+	await fs.rmrf("./out");
+	await fs.rmrf("./dist");
+
+	await fs.mkdirp("./out");
 	await fs.mkdirp("./dist");
+
 	await ctx.exec("webpack --mode=production");
+
 	const outZip = `./dist/turtlemay-target-${manifestJson.version}.zip`;
 	await ctx.exec(`zip -D -r ${outZip} ./css ./out ./manifest.json`);
 	await fs.copy(outZip, "./dist/turtlemay-target.zip");
