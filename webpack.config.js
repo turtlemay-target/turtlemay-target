fe41465bf764380a649d377f12a66b57cf1f04cc
Turtlemay <am@trtl.in>
2023-10-30 12:38:27 -0400

    Lowered webpack config objects into scope.

--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,60 @@ const manifestJson = require("./manifest.json");
 const packageJson = require("./package.json");
 const TerserPlugin = require("terser-webpack-plugin");
 
-const baseConfig = {
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
-	},
-};
-
-const extensionConfig = {
-	name: "extension",
-	entry: "./scripts/content.ts",
-	output: {
-		path: path.resolve(__dirname, "out"),
-		filename: "content.js",
-	},
-};
-
-const userscriptConfig = {
-	name: "userscript",
-	entry: "./scripts/content.ts",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: `${packageJson.name}.user.js`,
-	},
-	plugins: [
-		new TinyWebpackUserscriptPlugin({
-			scriptName: `${packageJson.name}-${manifestJson.version}`,
-			headers: [{
-				meta: {
-					name: manifestJson.name,
-					namespace: packageJson.repository,
-					description: manifestJson.description,
-					author: manifestJson.author,
-					icon: "https://www.google.com/s2/favicons?sz=64&domain=target.com",
-					version: manifestJson.version,
-					match: "https://*.target.com/*",
-				},
-			}],
-		}),
-	],
-};
-
 module.exports = (env, argv) => {
+	const baseConfig = {
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.tsx?$/,
+					use: "ts-loader",
+					exclude: /node_modules/,
+				},
+			],
+		},
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"],
+		},
+	};
+
+	const extensionConfig = {
+		name: "extension",
+		entry: "./scripts/content.ts",
+		output: {
+			path: path.resolve(__dirname, "out"),
+			filename: "content.js",
+		},
+	};
+
+	const userscriptConfig = {
+		name: "userscript",
+		entry: "./scripts/content.ts",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: `${packageJson.name}.user.js`,
+		},
+		plugins: [
+			new TinyWebpackUserscriptPlugin({
+				scriptName: `${packageJson.name}-${manifestJson.version}`,
+				headers: [{
+					meta: {
+						name: manifestJson.name,
+						namespace: packageJson.repository,
+						description: manifestJson.description,
+						author: manifestJson.author,
+						icon: "https://www.google.com/s2/favicons?sz=64&domain=target.com",
+						version: manifestJson.version,
+						match: "https://*.target.com/*",
+					},
+				}],
+			}),
+		],
+	};
+
 	if (argv.mode === "development") {
 		baseConfig.devtool = "inline-source-map";
 	}
