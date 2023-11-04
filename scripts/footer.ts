ee657cfbd47ca9c5935c86ed2714cc2b0c7b692c
Turtlemay <am@trtl.in>
2023-11-03 21:19:17 -0400

    Reduced indentation.

--- scripts/footer.ts
+++ scripts/footer.ts
@@ -14,27 +14,29 @@ void new MutationObserver(initFooter)
 	.observe(document.body, { childList: true, subtree: true });
 
 async function initFooter() {
-	if (!document.body.contains(elem)) {
-		// We will copy our styles from an existing link.
-		const refEl = document.querySelector(`[data-test*="@web/component-footer"] a[href*="terms-conditions"]`);
-
-		if (!refEl) {
-			return;
-		}
-
-		elem.innerHTML = `
-			<a class="turtlemay__footerText ${refEl.className}" href="${packageJson.repository}" target="_blank">
-				<span class="turtlemay__footerTextIcon">🧩</span>
-				<span>${manifestJson.name}</span>
-				<span class="turtlemay__footerTextVersion">${manifestJson.version}</span>
-			</a>
-		`;
-
-		const adjacentElem = document.querySelector(
-			'div[data-test="@web/component-footer/SubFooter"] ' +
-			'a[data-test="@web/component-footer/LegalLink"]:last-of-type'
-		);
-
-		adjacentElem?.insertAdjacentElement("afterend", elem);
+	if (document.body.contains(elem)) {
+		return;
 	}
+
+	// We will copy our styles from an existing link.
+	const refEl = document.querySelector(`[data-test*="@web/component-footer"] a[href*="terms-conditions"]`);
+
+	if (!refEl) {
+		return;
+	}
+
+	elem.innerHTML = `
+		<a class="turtlemay__footerText ${refEl.className}" href="${packageJson.repository}" target="_blank">
+			<span class="turtlemay__footerTextIcon">🧩</span>
+			<span>${manifestJson.name}</span>
+			<span class="turtlemay__footerTextVersion">${manifestJson.version}</span>
+		</a>
+	`;
+
+	const adjacentElem = document.querySelector(
+		'div[data-test="@web/component-footer/SubFooter"] ' +
+		'a[data-test="@web/component-footer/LegalLink"]:last-of-type'
+	);
+
+	adjacentElem?.insertAdjacentElement("afterend", elem);
 }
